97567fe21c0dda91440f40e95e9e4b0a78fb1216
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-08-11 21:37:22 -0300

    ATT: Inicio da atividade 2

---
+++ projetos/atividade002-Funcoes-php/g-conversor-temperatura/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+    <meta charset="UTF-8">
+    <title>Operadores Aritméticos</title>
+    <link rel="stylesheet" href="public/css/estilo.css">
+    </head>
+<body>
+
+    <header>
+
+        <h1></h1>
+
+    </header>
+
+    <main>
+
+        <form class="formulario">
+            <label for=""></label><br>
+            <input type="" id="" name="" required><br>
+
+            <label for=""></label><br>
+            <input type="" id="" name="" required><br>
+
+            <label for="">Escolha a operação:</label><br>
+            <select id="" name="">
+                <option value="">--Selecione--</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select><br><br>
+
+            <button type="submit">Verificar</button>
+        </form>    
+    
+    </main>
+
+</body>
+</html>
